ca7a53aa93aeea19b93e85eb25de648ad8cae8cf
MishaSabre <mikhailsablin229@gmail.com>
2022-10-26 21:28:43 +0800

    change Person

--- src/Person.java
+++ src/Person.java
@@ -7,14 +7,9 @@ public class Person {
     private int mailInd;
     private long number;
 
-   /* Person(int id, String name, String surname, int mailInd, long number){
+    Person(){
         count++;
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-        this.mailInd = mailInd;
-        this.number = number;
-    }*/
+    }
     public void setId(int id){
         this.id = id;
     }
